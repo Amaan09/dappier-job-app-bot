d1a55c2a878f699770b1171a45c3d2ad2aec03e4
Mohammed Amaan <mohammedamaan79@gmail.com>
2024-10-03 23:32:51 +0530

    setup routes for training model and chat completion of the rag model

---
+++ src/routes/resume.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, request, jsonify
+from ..services import resume_service
+from ..types import TrainModelRequest, ChatCompletionRequest
+
+bp = Blueprint('resume', __name__, url_prefix='/resume')
+
+@bp.route("/train_model", methods=['POST']) 
+def train_model():
+    data = request.get_json()
+    train_model_request = TrainModelRequest(**data)
+    response = resume_service.train_model(train_model_request)
+    return jsonify(response.to_dict()), 200
+
+@bp.route("/chat_completion", methods=['POST'])
+def chat_completion():
+    
+    data = request.get_json();
+    chat_completion_request = ChatCompletionRequest(**data)
+    response = resume_service.chat_completion(chat_completion_request)
+    return jsonify(response.to_dict()), 200
