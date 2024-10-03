b04c69796c877649526da84a59628d77d9371957
Mohammed Amaan <mohammedamaan79@gmail.com>
2024-10-04 00:21:55 +0530

    add cors to the flask app

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,13 @@
-from flask import Flask
-from .routes import resume
 from dotenv import load_dotenv
+from flask import Flask
+from flask_cors import CORS
+from .routes import resume
 
 load_dotenv()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
 
     # Register blueprints
     app.register_blueprint(resume.bp)
