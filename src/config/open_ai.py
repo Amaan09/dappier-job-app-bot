96abd78993589d337cf1f63e0bff49365b354ffb
Mohammed Amaan <mohammedamaan79@gmail.com>
2024-10-04 14:38:50 +0530

    test to deploy app on heroku again

--- src/config/open_ai.py
+++ src/config/open_ai.py
@@ -4,6 +4,6 @@ from langchain_openai import ChatOpenAI
 model = os.environ.get("OPENAI_MODEL")
 
 if not model:
-    raise ValueError("OPENAI_MODEL is not set in the environment variables.")
+    raise ValueError("OPENAI_MODEL is not set in the environment variables")
 
 openai_model = ChatOpenAI(model=model, temperature=1)
